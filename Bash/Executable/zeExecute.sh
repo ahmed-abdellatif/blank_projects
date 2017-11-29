008e8b812d9cafb23399e477ec962d38bbb82bf1
Logan Mancuso <lmancuso@expresscomputer.biz>
2017-11-29 10:21:21 -0500

    bash commit

--- Bash/Executable/zeExecute.sh
+++ Bash/Executable/zeExecute.sh
@@ -7,19 +7,28 @@
 # *************************************************************
 
 #!/bin/bash -x
+YES_OR_NO="Invalid please type [Y]es or [N]o"
 echo "Descend into 'TestDirectory' directory"
 cd ../../TestDirectory/FolderStructure/
-include=../../IOFiles/
-#
-for item in *
-do
-  echo " "
-  echo "EXECUTING" $item
-  # cd $item
-  bashdb ../SourceFiles/Aprog.sh $include/xin.txt | tee $include/xlog.txt
-  # cd ..
+include="../../IOFiles/"
+
+sleep .5
+read -p "Debugging...? [Y]es/[N]o   " yn_
+case "$yn_" in
+  [yY][eE][sS]|Y|y ) # yes
+    echo "Executing with debugging"
+    bashdb ../SourceFiles/Aprog.sh $include/xin.txt | tee $include/xlog.txt
+  ;;
+  [nN][oO]|N|n ) # no
+    echo "Executing without debugging"
+    ../SourceFiles/Aprog.sh $include/xin.txt | tee $include/xlog.txt
+  ;;
+  * ) # else
+    echo $YES_OR_NO
+  ;;
+esac
+
 echo "EXECUTION COMPLETE"
-done
 echo "Return from 'TestDirectory' directory"
 cd ../../
 echo " "
