21390f09a9b8474918f4e24f95ca1622f9379998
logmancuso <lmancuso@usc-sc.edu>
2017-11-29 10:07:43 -0500

    date refresh

--- tmp.sh
+++ tmp.sh
@@ -14,6 +14,26 @@ for i in *
         fi
 done
 }
+today=$(date +%m-%d-%Y--%H:%M:%S) to_replace="Last Edit Date:"
+#loop to replace last edit date
+other_loop() {
+  for item in *
+  do
+    if [[ -f $item ]];
+    then
+      echo "File $item"
+      sed "0,\|.* $to_replace.*|{s|$to_replace.*|$to_replace $today|g1}" $item
+    elif [[ -d $item ]];
+    then
+      echo "Directory $item"
+      cd $item
+      other_loop
+      cd ../
+    else
+      echo "Error $item"
+    fi
+  done
+}
 
 for i in *
 	do
@@ -21,11 +41,13 @@ for i in *
 	then
 		echo "file" $i
 		dos2unix $i
+    sed "0,\|.* $to_replace.*|{s|$to_replace.*|$to_replace $today|g1}" $i
 	elif [[ -d $i ]];
 	then
 		echo "dir" $i
 		cd $i
-		loop
+		# loop
+    other_loop
 		cd ../
 	fi
 done
